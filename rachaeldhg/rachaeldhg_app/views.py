8cde2f22a8737ebc1216bd35ee08d13ddb14a62f
dreambender <anthony.green5140@gmail.com>
2021-07-05 20:11:42 -0700

    first push rdhg

---
+++ rachaeldhg/rachaeldhg_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def services(request):
+    return render(request, 'about_me.html')
